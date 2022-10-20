3d913f43260ca003ff9071d2924fb845cea7b600
iniagbo <mzeepositive@gmail.com>
2022-10-20 14:12:22 -0700

    3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10-Make alphabet x10 times
  *
@@ -8,13 +9,14 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
-	while (i <= 9)
 
+	while (i <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
+
 		_putchar('\n');
 		i++;
 	}
