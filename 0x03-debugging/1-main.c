c11b81105d25509dead6617fdf28068347e58c96
iniagbo <mzeepositive@gmail.com>
2022-10-21 16:28:44 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,18 +2,12 @@
 
 
 
-/**
- *
- *  * main-causes an infinite loop
- *
- *   * Return: 0
- *
- *    */
-
-
+/*
+ * main-causes an infinite loop
+ * Return: 0
+ */
 
 int main(void)
-
 {
 
 		int i;
