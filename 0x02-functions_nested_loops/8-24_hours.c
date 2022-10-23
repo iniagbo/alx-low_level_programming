604f8746ce8d37d00aea75f513ce5e0ba174e707
iniagbo <mzeepositive@gmail.com>
2022-10-23 10:12:28 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,65 +1,46 @@
-#include "main.h"
+#include "stdio.h"
 
 /**
- *
- *  * jack_bauer-prints all minutes in a day from 00:00
- *
- *   *
- *
- *    * Return:0 if successful.
- *
- *     */
-
-
-
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
+ * Return: void
+ */
 void jack_bauer(void)
-
 {
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-		int i, j, k, m;
-
-
-
-			for (i = 0; i <= 2; i++)
-
-					{
-
-								for (j = 0; j <= 9; j++)
-
-											{
-
-															for (k = 0; k <= 5; k++)
-
-																			{
-
-																								for (m = 0; m <= 9; m++)
-
-																													{
-
-																																			if (i == 2 && j > 3)
-
-																																									{break;
-
-																																															}
-
-																																								_putchar(i + '0');
-
-																																													_putchar(j + '0');
-
-																																																		_putchar(':');
-
-																																																							_putchar(k + '0');
-
-																																																												_putchar(m + '0');
-
-																																																																	_putchar('\n');
-
-																																																																					}
-
-																											}
-
-																	}
-
-									}
-
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
+	{
+		if (hours_tens == '2')
+		{
+			hours_max = '4';
+		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
+	}
 }
