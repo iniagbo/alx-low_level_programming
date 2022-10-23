e357627b8d08f5be6ff67af3e536b343051ff5ea
iniagbo <mzeepositive@gmail.com>
2022-10-23 08:26:00 -0700

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,75 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table-Prints the n times table
- * @n: takes number input
+ * main-Programme Entry Point.
  *
- *     */
+ * Description: computes the sum of all
+ * the multiples of 3 or 5
+ * below 1024 (excluded)
+ *
+ * Return: Always 0.
+ */
 
-
-
-void print_times_table(int n)
-
+int main(void)
 {
+	int sum, num;
 
-		int prod, mult, num;
-
-
-
-			if (n <= 15 && n >= 0)
-
-					{
-
-								for (num = 0; num <= n; ++num)
-
-											{
-
-															_putchar(48);
-
-																		for (mult = 1; mult <= n; ++mult)
-
-																						{
-
-																											_putchar(',');
-
-																															_putchar(' ');
-
-
-
-																																			prod = num * mult;
-
-
-
-																																							if (prod <= 9)
-
-																																													_putchar(' ');
-
-																																											if (prod <= 99)
-
-																																																	_putchar(' ');
-
-
-
-																																															if (prod >= 100)
-
-																																																				{
-
-																																																										_putchar((prod / 100) + 48);
-
-																																																															_putchar((prod / 10) % 10 + 48);
-
-																																																																			} else if (prod <= 99 && prod >= 10)
-
-																																																																									_putchar((prod / 10) + 48);
-
-																																																																							_putchar((prod % 10) + 48);
-
-																																																																										}
-
-																					_putchar('\n');
-
-																							}
-
-									}
-
+	for (num = 0; num < 1024; ++num)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
